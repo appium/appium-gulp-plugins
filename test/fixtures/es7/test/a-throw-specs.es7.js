786d07fe5916c8c37a8f84c7f794d6ad7dc907d8
dan@saucelabs.com <dan@saucelabs.com>
2018-03-26 17:54:58 -0700

    No mocha arrows

--- test/fixtures/es7/test/a-throw-specs.es7.js
+++ test/fixtures/es7/test/a-throw-specs.es7.js
@@ -5,8 +5,8 @@ import {A} from '../lib/a';
 
 chai.should();
 
-describe('a-throw', () => {
-  it('should throw', () => {
+describe('a-throw', function () {
+  it('should throw', function () {
     let a = new A('hello world!');
     a.throwError('This is really bad!');
   });

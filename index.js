2062a40ffa15d1e129964b355d7e29f1a377d124
Jonathan Lipps <jlipps@gmail.com>
2019-01-09 14:16:19 -0800

    add support for typescript projects (#48)
    * add support for typescript projects
    
    * add ability to generate typings
    
    * better handling of lintOnWatch logic
    
    * fix lint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.Transpiler = require('./lib/transpiler');
+exports.TsTranspiler = require('./lib/transpiler-ts');
 exports.spawnWatcher = require('./lib/spawn-watcher');
 exports.boilerplate = require('./lib/boilerplate');
 

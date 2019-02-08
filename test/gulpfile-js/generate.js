6e9a54c31f252ddfc0cfa4953280021eeaed5e2e
Isaac Murchie <isaac.appium@gmail.com>
2019-02-08 16:19:43 -0500

    Add logging to ios build tasks

--- test/gulpfile-js/generate.js
+++ test/gulpfile-js/generate.js
@@ -4,9 +4,8 @@ const gulp = require('gulp');
 const { Transpiler, TsTranspiler, spawnWatcher, isVerbose } = require('../..');
 const _ = require('lodash');
 const B = require('bluebird');
-const rimraf = B.promisify(require('rimraf'));
-const exec = B.promisify(require('child_process').exec);
-const glob = B.promisify(require('glob'));
+const { fs } = require('appium-support');
+const { exec } = require('teen_process');
 const assert = require('assert');
 const debug = require('gulp-debug');
 const gulpIf = require('gulp-if');
@@ -15,18 +14,18 @@ const gulpIf = require('gulp-if');
 spawnWatcher.use(gulp);
 
 gulp.task('generate-lots-of-files', async function () {
-  await rimraf('test/generated/es7 test/generated/ts build/generated');
-  await exec('mkdir -p test/generated/es7');
-  await exec('mkdir -p test/generated/ts');
+  await fs.rimraf('test/generated/es7 test/generated/ts build/generated');
+  await exec('mkdir', ['-p', 'test/generated/es7']);
+  await exec('mkdir', ['-p', 'test/generated/ts']);
   await B.all([
     ...(
       _.times(24).map(function (i) {
-        return exec(`cp test/fixtures/es7/lib/a.es7.js test/generated/es7/a${i + 1}.es7.js`);
+        return exec('cp', ['test/fixtures/es7/lib/a.es7.js', `test/generated/es7/a${i + 1}.es7.js`]);
       })
     ),
     ...(
       _.times(24).map(function (i) {
-        return exec(`cp test/fixtures/ts/lib/b.ts test/generated/ts/b${i + 1}.ts`);
+        return exec('cp', ['test/fixtures/ts/lib/b.ts', `test/generated/ts/b${i + 1}.ts`]);
       })
     ),
   ]);
@@ -55,26 +54,26 @@ gulp.task('transpile-lots-of-files',
 );
 
 gulp.task('test-transpile-lots-of-es7-files', async function testTranspileLotsOfFiles () {
-  let files = await glob('test/generated/es7/**/*.js');
+  let files = await fs.glob('test/generated/es7/**/*.js');
   const numOfFiles = files.length;
   assert(numOfFiles > 16);
 
-  files = await glob('build/generated/a*.js');
+  files = await fs.glob('build/generated/a*.js');
   assert(files.length === numOfFiles);
 
-  files = await glob('build/generated/*.es7.js');
+  files = await fs.glob('build/generated/*.es7.js');
   assert(files.length === 0);
 });
 
 gulp.task('test-transpile-lots-of-ts-files', async function testTranspileLotsOfFiles () {
-  let files = await glob('test/generated/ts/**/*.ts');
+  let files = await fs.glob('test/generated/ts/**/*.ts');
   const numOfFiles = files.length;
   assert(numOfFiles > 16);
 
-  files = await glob('build/generated/b*.js');
+  files = await fs.glob('build/generated/b*.js');
   assert(files.length === numOfFiles);
 
-  files = await glob('build/generated/*.ts');
+  files = await fs.glob('build/generated/*.ts');
   assert(files.length === 0);
 });
 

27d7de0acd01151674ffac7ee1f9a984a63cb345
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-05-09 17:55:43 +0900

    got rid of tslint/typescript stuff

--- test/fixtures/ts/lib/b-run.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable:no-console */
-import { B } from './b';
-
-
-const b = new B('hello world!');
-
-console.log(b.getText());
--- test/fixtures/ts/test/b-throw-specs.ts
+++
@@ -1,9 +0,0 @@
-import { B } from '../lib/b';
-
-
-describe('b-throw', function () {
-  it('should throw', function () {
-    const b = new B('hello world!');
-    b.throwError('This is really bad!');
-  });
-});
